bddfcf360f53b89a48b77cceaa5dd539b8ed12ec
Olga <ofkjh@fkdj.com>
2023-01-21 10:17:45 +0200

    Initial commit

---
+++ peregruzka_operatorov.py
@@ -0,0 +1,22 @@
+class o_int:
+    number = int()
+
+    def __init__(self, number):
+        self.number = number
+
+    def __str__(self):
+        return f'{self.number}'
+
+    def __add__(self, other, number):
+        if other.number == 0:
+            return self.number + number
+
+
+a = o_int(1)
+b = o_int(1)
+print(a+5)
+
+
+
+
+
